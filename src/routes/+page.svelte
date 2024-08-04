fa2877fb615ccbccea84622210c5928fbb1e3627
Alper Reha Yazgan <alperreha@gmail.com>
2024-08-04 15:16:06 +0300

    [v0.0.1]: gh-pages template to svelte conversion made.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let count = 0;
+</script>
+
+<h1>Alper Reha Blog</h1>
+
+<p>Count: {count}</p>
+<button on:click={() => (count -= 1)}>Decrement</button>
+<button on:click={() => (count += 1)}>Increment</button>
