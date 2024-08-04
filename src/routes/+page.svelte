6d847fb335fd51ce8972d378e990c1c53eddc92f
Alper Reha Yazgan <alperreha@gmail.com>
2024-08-04 15:36:13 +0300

    [gh-pages]: svelte, tailwindcss and pages static adapter boilerpalte ready.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,78 @@
 	let count = 0;
 </script>
 
-<h1>Alper Reha Blog</h1>
+<!-- Simple full scren and centered div (row style) -->
+<div class="flex items-center justify-center h-screen bg-gray-900">
+	<div>
+		<div class="flex flex-col items-center text-center justify-center">
+			<img
+				class="w-32 h-32 rounded-full"
+				src="https://avatars.githubusercontent.com/u/43668181?v=4"
+				alt="Alper Reha YAZGAN"
+			/>
+			<h1 class="mt-4 text-3xl font-bold text-white">Alper Reha YAZGAN</h1>
+			<p class="mt-2 text-sm text-gray-400">
+				Software Engineer at
+				<a href="https://www.turktelekom.com.tr" class="text-blue-400">Turk Telekom</a>
+				via <a href="https://www.gantek.com" class="text-blue-400">Gantek</a>
+			</p>
+		</div>
+		<div class="flex flex-col items-center text-center justify-center mt-4">
+			<div class="w-3/4">
+				<p class="text-sm text-gray-400">
+					I am a software developer who is passionate about making open-source more accessible,
+					creating technology to elevate people, and building community. Some technologies I enjoy
+					working with include DevOps, CI/CD Pipelines and Cloud Computing. In development I mainly
+					use <a href="https://go.dev" class="text-blue-400">Go</a> and
+					<a href="https://www.nodejs.org" class="text-blue-400">Node.js</a>.
+				</p>
 
-<p>Count: {count}</p>
-<button on:click={() => (count -= 1)}>Decrement</button>
-<button on:click={() => (count += 1)}>Increment</button>
+				<p class="mt-2 text-sm text-gray-400">
+					I am currently working as a software developer at <a
+						href="https://www.turktelekom.com.tr"
+						class="text-blue-400">Turk Telekom</a
+					>
+					via <a href="https://www.gantek.com" class="text-blue-400">Gantek</a>. I am also a student
+					at
+					<a href="https://www.yildiz.edu.tr" class="text-blue-400">Yildiz Technical University</a>.
+				</p>
+			</div>
+		</div>
+		<!-- Socials in one line multi icon -->
+		<div class="flex items-center justify-center mt-4">
+			<a class="text-gray-400 hover:text-blue-500" href="https://twitter.com/alperreha"
+				><i class="fab fa-twitter"></i></a
+			>
+			<a class="ml-4 text-gray-400 hover:text-blue-500" href="https://github.com/alperrehayazgan"
+				><i class="fab fa-github"></i></a
+			>
+			<a class="ml-4 text-gray-400 hover:text-blue-500" href="https://linkedin.com/in/alperreha"
+				><i class="fab fa-linkedin"></i></a
+			>
+			<a class="ml-4 text-gray-400 hover:text-blue-500" href="https://youtube.com/c/alperrehayazgan"
+				><i class="fab fa-youtube"></i></a
+			>
+		</div>
+	</div>
+
+	<!-- Footer for social links again with copyrights fixed-bottom and max 60px -->
+	<div class="fixed bottom-0 w-full h-12 bg-gray-900">
+		<div class="flex items-center justify-center">
+			<a class="text-gray-400 hover:text-blue-500" href="https://twitter.com/alperreha"
+				><i class="fab fa-twitter"></i></a
+			>
+			<a class="ml-4 text-gray-400 hover:text-blue-500" href="https://github.com/alperrehayazgan"
+				><i class="fab fa-github"></i></a
+			>
+			<a class="ml-4 text-gray-400 hover:text-blue-500" href="https://linkedin.com/in/alperreha"
+				><i class="fab fa-linkedin"></i></a
+			>
+			<a class="ml-4 text-gray-400 hover:text-blue-500" href="https://youtube.com/c/alperrehayazgan"
+				><i class="fab fa-youtube"></i></a
+			>
+		</div>
+		<div class="flex items-center justify-center mt-4">
+			<p class="text-sm text-gray-400">© 2022 Alper Reha YAZGAN</p>
+		</div>
+	</div>
+</div>
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,32 @@
+<!-- Space height 120px -->
+<div style="height: 120px;"></div>
+
+<!-- div for contents -->
+<!-- content will be fetched from jquery. Until its done. Show loading -->
+<div id="content" class="text-white text-center">
+	<i class="fas fa-spinner fa-spin fa-3x" style="color: #4FD1C5;"></i>
+</div>
+
+<!-- Space 25 px -->
+<div style="height: 50px;"></div>
+
+<!-- Footer for social links again with copyrights fixed-bottom and max 60px -->
+<div class="flex items-center justify-center mt-4">
+	<div class="w-1/2">
+		<div class="flex items center justify-center">
+			<a class="text-gray-400 hover:text-blue-500" href="https://twitter.com/alperreha"
+				><i class="fab fa-twitter"></i></a
+			>
+			<a class="ml-4 text-gray-400 hover:text-blue-500" href="https://github.com/alperrehayazgan"
+				><i class="fab fa-github"></i></a
+			>
+			<a class="ml-4 text-gray-400 hover:text-blue-500" href="https://linkedin.com/in/alperreha"
+				><i class="fab fa-linkedin"></i></a
+			>
+		</div>
+		<div class="flex items-center justify-center mt-4">
+			<p class="text-sm text-gray-400">© 2022 Alper Reha YAZGAN</p>
+		</div>
+		<div style="height: 50px;"></div>
+	</div>
+</div>
